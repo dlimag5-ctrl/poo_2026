c8b50993b8df2eaf5c0dc4b7d1937c1db4585814
dlimag5-ctrl <dlimag5@miumg.edu.gt>
2026-02-15 09:52:18 -0600

    se agrego la clase persona

---
+++ Persona.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Persona{
+	//atributos
+	protected :
+		string nombres,apellidos,direccion,fecha_nacimiento;
+		int telefono;
+		//Constructor
+		Persona(){
+		}
+		Persona(string nom,string ape,string dir,int tel,string fn){
+			nombres = nom;
+			apellidos = ape;
+			direccion = dir;
+			telefono = tel;
+			fecha_nacimiento = fn;	
+		}
+		//metodos (crud)
+		void crear();
+		void leer();
+		void actualizar();
+		void borrar();		
+};
